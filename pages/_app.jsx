5b255ca90e840fd3373e2a2f8d55e2942918f38c
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-28 14:30:42 -0300

    Pages - app, document, login and signUp

---
+++ pages/_app.jsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import theme from '../src/theme'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: 'Itim', cursive;
+    background-color: ${(props) => props.theme.backgroundColor};
+  }
+`
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
