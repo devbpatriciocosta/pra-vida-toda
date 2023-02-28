9faf8e00e0411b3793864fbb077b8b434d5f4341
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-28 16:04:25 -0300

    Theme creation

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  primary: '#FAE3D9',
+  secondary: '#FFB6B9',
+  terciary: '#8AC6D1',
+  primaryFontColor: '#FFFFFF',
+  secondaryFontColor: '#FFB6B9',
+  backgroundColor: '#FAE3D9',
+  white: '#FFFFFF'
+}
+
+export default theme
