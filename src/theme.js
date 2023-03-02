868f504af94268a831e831f0cff67500b52723bb
Bruno Patrício <bpatriciocosta@gmail.com>
2023-03-02 15:26:22 -0300

    Theme color adjust

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,8 @@ const theme = {
   primary: '#FAE3D9',
   secondary: '#FFB6B9',
   terciary: '#8AC6D1',
-  primaryFontColor: '#FFFFFF',
+  quaternary: '#BBDED6',
+  primaryFontColor: '#BBDED6',
   secondaryFontColor: '#FFB6B9',
   backgroundColor: '#FAE3D9',
   white: '#FFFFFF'
